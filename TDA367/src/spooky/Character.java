315a2ed8880b15c4e65fa4787b5daa9478f6cc28
Kaffe-work <leo.k.oinonen@gmail.com>
2017-04-03 14:53:33 +0200

    a

---
+++ TDA367/src/spooky/Character.java
@@ -0,0 +1,15 @@
+package spooky;
+
+/**
+ * Created by kaffe on 4/3/17.
+ */
+public class Character extends WorldObject{
+
+    public int health = 1;
+    public int speed = 1;
+
+
+
+
+
+}
---
+++ TDA367/src/spooky/Upgrades.java
@@ -0,0 +1,7 @@
+package spooky;
+
+/**
+ * Created by kaffe on 4/3/17.
+ */
+public class Upgrades {
+}
