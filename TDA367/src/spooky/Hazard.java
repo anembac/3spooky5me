ca6803751c4292d0d8c711b0379526143d829b4c
miktor <m.tornqvist@hotmail.com>
2017-04-03 14:42:58 +0200

    a

---
+++ TDA367/src/spooky/Hazard.java
@@ -0,0 +1,8 @@
+package spooky;
+
+/**
+ * Created by miktor on 2017-04-03.
+ */
+public class Hazard {
+
+}
---
+++ TDA367/src/spooky/WorldObject.java
@@ -0,0 +1,14 @@
+package spooky;
+
+/**
+ * Created by miktor on 2017-04-03.
+ */
+public abstract class WorldObject {
+    public WorldObject(){}
+
+    public void Render(){}
+
+    public void Frame() {}
+
+
+}
