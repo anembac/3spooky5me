89f78068b4071cedf97faea44603857c9407eef0
mintuw <jacob.rohdin@gmail.com>
2017-04-05 14:31:08 +0200

    Added a getDamageable in the character class and a comment.

--- TDA367/src/spooky/Character.java
+++ TDA367/src/spooky/Character.java
@@ -3,21 +3,24 @@ package spooky;
 /**
  * Created by kaffe on 4/3/17.
  */
-public class Character extends WorldObject{
+public class Character extends WorldObject {
 
     protected int health = 100;
-    protected int speed = 10;
+    protected int speed = 100; //This speed equals 1 block per second.
     protected boolean damagable = true;
 
 
-    private final int getHealth()
-    {
+    private final int getHealth() {
         return health;
     }
 
-    private final int getSpeed()
-    {
+    private final int getSpeed() {
         return speed;
     }
+
+    private final boolean getDamageable(){
+        return damagable;
+    }
+
 
 }
