c3d35cf0e88744b19c1c9263fc85795c5f4b8fc8
anembac <anembac@gmail.com>
2017-04-25 16:02:51 +0200

    Added libgdx

---
+++ InfinityRun/desktop/src/com/tda367/infinityrun/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tda367.infinityrun.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tda367.infinityrun.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
