21c88e5038345e1db251995fd872e2bbabc3970c
mintuw <jacob.rohdin@gmail.com>
2017-05-03 15:30:34 +0200

    Removed some comments + CharacterID from the Upgrades

--- TDA367/src/spooky/Upgrades.java
+++ TDA367/src/spooky/Upgrades.java
@@ -3,12 +3,10 @@ package spooky;
 import java.util.List;
 
 public class Upgrades {
-    private int characterID;
     private List<Upgrade> upgradesList;
 
 
     public Upgrades(int characterID) {
-        this.characterID = characterID;
         upgradesList.add(0, new Upgrade("Health Points", -1, false, 20));
         upgradesList.add(1, new Upgrade("Melee Weapon Handling", -1, true, 5));
         upgradesList.add(2, new Upgrade("Ranged Weapon Handling", -1, true, 5));
