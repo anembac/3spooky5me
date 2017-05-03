0b9aec68527fabf979f66917fde293edd7fc2415
mintuw <jacob.rohdin@gmail.com>
2017-05-03 15:11:29 +0200

    Updated upgrades.

--- TDA367/src/spooky/Upgrade.java
+++ TDA367/src/spooky/Upgrade.java
@@ -1,24 +1,41 @@
 package spooky;
 
-/**
- * Created by Jacob on 5/3/2017.
- */
 public class Upgrade {
 
     String upgradeName;
     int cap;
     boolean multiply;
-    int value;
+    int basevalue;
     int level = 0;
 
-    public Upgrade (String upgradeName, int cap, boolean multiply, int value) {
+    public Upgrade(String upgradeName, int cap, boolean multiply, int basevalue) {
         this.upgradeName = upgradeName;
         this.cap = cap;
         this.multiply = multiply;
-        this.value = value;
+        this.basevalue = basevalue;
     }
 
-    public void levelUp (){
+    public void levelUp() {
         this.level++;
     }
+
+    public String getUpgradeName() {
+        return this.upgradeName;
+    }
+
+    public int getCap() {
+        return this.cap;
+    }
+
+    public int getValue() {
+        if (this.multiply) {
+            return 100 + this.basevalue * this.level;
+        }
+        return this.basevalue * this.level;
+    }
+
+    public boolean doMultiply() {
+        return this.multiply;
+    }
+
 }
--- TDA367/src/spooky/Upgrades.java
+++ TDA367/src/spooky/Upgrades.java
@@ -1,14 +1,19 @@
 package spooky;
 
-/**
- * Created by kaffe on 4/3/17.
- */
-public class Upgrades{
+import java.util.List;
+
+public class Upgrades {
     private int characterID;
+    private List<Upgrade> upgradesList;
 
 
-    public Upgrades( int characterID) {
+    public Upgrades(int characterID) {
         this.characterID = characterID;
-
+        upgradesList.add(0, new Upgrade("Health Points", -1, false, 20));
+        upgradesList.add(1, new Upgrade("Melee Weapon Handling", -1, true, 5));
+        upgradesList.add(2, new Upgrade("Ranged Weapon Handling", -1, true, 5));
+        upgradesList.add(3, new Upgrade("Health Regeneration", -1, false, 1));
+        upgradesList.add(4, new Upgrade("Quiver Size", -1, false, 5));
+        upgradesList.add(5, new Upgrade("Better Looting", -1, true, 2));
     }
 }
