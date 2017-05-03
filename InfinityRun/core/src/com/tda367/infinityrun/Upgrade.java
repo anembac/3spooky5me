8b9f80eb0c112a3841002fdd0d608781de2ed60f
mintuw <jacob.rohdin@gmail.com>
2017-05-03 15:35:29 +0200

    Moved to the correct directory.

---
+++ InfinityRun/core/src/com/tda367/infinityrun/Upgrade.java
@@ -0,0 +1,41 @@
+package com.tda367.infinityrun;
+
+public class Upgrade {
+
+    String upgradeName;
+    int cap;
+    boolean multiply;
+    int basevalue;
+    int level = 0;
+
+    public Upgrade(String upgradeName, int cap, boolean multiply, int basevalue) {
+        this.upgradeName = upgradeName;
+        this.cap = cap;
+        this.multiply = multiply;
+        this.basevalue = basevalue;
+    }
+
+    public void levelUp() {
+        this.level++;
+    }
+
+    public String getUpgradeName() {
+        return this.upgradeName;
+    }
+
+    public int getCap() {
+        return this.cap;
+    }
+
+    public int getValue() {
+        if (this.multiply) {
+            return 100 + this.basevalue * this.level;
+        }
+        return this.basevalue * this.level;
+    }
+
+    public boolean doMultiply() {
+        return this.multiply;
+    }
+
+}
