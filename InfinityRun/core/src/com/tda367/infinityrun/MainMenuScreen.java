84f85b620e6920fb2a25725f74ab9a61069956b7
anembac <anembac@gmail.com>
2017-05-09 16:51:09 +0200

    Created basic main menu with buttons

--- InfinityRun/core/src/com/tda367/infinityrun/MainMenuScreen.java
+++ InfinityRun/core/src/com/tda367/infinityrun/MainMenuScreen.java
@@ -1,20 +1,60 @@
 package com.tda367.infinityrun;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 public class MainMenuScreen implements Screen { //this class creates the main menu screen, as shown in
                                                 // https://github.com/libgdx/libgdx/wiki/Extending-the-simple-game
     final InfinityRun game;
-
+    Stage mainMenuStage = new Stage();
     OrthographicCamera camera;
+
+
+    Texture packedtexture = new Texture(Gdx.files.internal("testpack1.png"));
+    TextureAtlas ta = new TextureAtlas(Gdx.files.internal("testpack1.pack"));
+    TextureRegion tr = new TextureRegion(ta.findRegion("testtexture"));
+    TextureRegion tr2 = new TextureRegion(ta.findRegion("testtexture2"));
+    TextureRegionDrawable trd = new TextureRegionDrawable(tr);
+    TextureRegionDrawable trd2 = new TextureRegionDrawable(tr2);
+    TextButton.TextButtonStyle tbs1;
+    TextButton newCharButton;
+    TextButton loadCharButton;
+    VerticalGroup buttonGroup;
+
 
     public MainMenuScreen(final InfinityRun game) {
         this.game = game;
+        Gdx.input.setInputProcessor(mainMenuStage);
+        tbs1 = new TextButton.TextButtonStyle(trd, trd2, trd, game.font);
+        newCharButton = new TextButton("NEW CHARACTER", tbs1);
+        loadCharButton = new TextButton("LOAD CHARACTER", tbs1);
+        buttonGroup = new VerticalGroup();
+        buttonGroup.space(10);
+        buttonGroup.setX(1600/2-buttonGroup.getWidth()/2);
+        buttonGroup.setY(900/2-buttonGroup.getHeight()/2);
+        buttonGroup.addActor(newCharButton);
+        buttonGroup.addActor(loadCharButton);
 
+        //ADD TO STAGE
+
+        mainMenuStage.addActor(buttonGroup);
+
+        //Camera stuff
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 1600, 900);
 
@@ -26,19 +66,22 @@ public class MainMenuScreen implements Screen { //this class creates the main me
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0.2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
         camera.update();
-        game.batch.setProjectionMatrix(camera.combined);
+        mainMenuStage.draw();
 
+        //Batch&Font rendering
+        game.batch.setProjectionMatrix(camera.combined);
         game.batch.begin();
         game.font.draw(game.batch, "Welcome to InfinityRun PRE-ALPHA!!! ", 100, 150);
-        game.font.draw(game.batch, "Press any key to begin!", 100, 100);
+        game.font.draw(game.batch, "Press \"NEW CHARACTER\" to begin!", 100, 100);
         game.batch.end();
 
-        if (Gdx.input.isKeyPressed(Input.Keys.ANY_KEY)) {
+
+        if(newCharButton.isPressed()){
+            this.dispose();
             game.setScreen(new GameScreen(game));
-            dispose();
         }
+
     }
 
     @Override
@@ -66,6 +109,12 @@ public class MainMenuScreen implements Screen { //this class creates the main me
 
     @Override
     public void dispose() {
+
+        mainMenuStage.clear();
+        buttonGroup.clear();
+        mainMenuStage.dispose();
+        ta.dispose();
+        packedtexture.dispose();
 
     }
 
