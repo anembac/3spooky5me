d3a4df05533116ec368ee77480cef82a7e60ab67
Kaffe-work <leo.k.oinonen@gmail.com>
2017-05-17 16:05:59 +0200

    Added a list of worldobjects in the roomTemplates.

--- InfinityRun/core/src/com/tda367/infinityrun/Roomtemplates/RoomUDL.java
+++ InfinityRun/core/src/com/tda367/infinityrun/Roomtemplates/RoomUDL.java
@@ -1,18 +1,61 @@
 package com.tda367.infinityrun.Roomtemplates;
 
+import com.tda367.infinityrun.Math.Vec2;
+import com.tda367.infinityrun.RoomTiles.BrickObject;
+import com.tda367.infinityrun.RoomTiles.Platform;
+
 /**
  * Created by kaffe on 5/12/17.
  */
 public class RoomUDL  extends RoomTemplate {
     public RoomUDL() {
-        u =true;
-        r=false;
-        d=true;
-        l=true;
+        u = true;
+        r = false;
+        d = true;
+        l = true;
         roomExits = 3;
-    }
+        for (int i = 0; i < 5; i++)
 
+        {
+            for (int j = 0; j < 5; j++) {
+
+                //bottom left bricks
+                roomObjects.add(new BrickObject(new Vec2(0 + i * 64, 0 + j * 64)));
+                roomObjects.add(new BrickObject(new Vec2(5 * block + i * block, 0 * block + j * block)));
+                // top left bricks
+                roomObjects.add(new BrickObject(new Vec2(0 * block + i * 64, 10 * block + j * 64)));
+                roomObjects.add(new BrickObject(new Vec2(5 * block + i * 64, 10 * block + j * 64)));
+
+
+                //right wall of bricks
+                roomObjects.add(new BrickObject(new Vec2(15 * block + i * 64, 0 + j * 64)));
+                roomObjects.add(new BrickObject(new Vec2(15 * block + i * 64, 5 * block + j * 64)));
+                roomObjects.add(new BrickObject(new Vec2(20 * block + i * 64, 0 + j * 64)));
+                roomObjects.add(new BrickObject(new Vec2(20 * block + i * 64, 5 * block + j * 64)));
+                roomObjects.add(new BrickObject(new Vec2(20 * block + i * 64, 10 * block + j * 64)));
+                roomObjects.add(new BrickObject(new Vec2(15 * block + i * 64, 10 * block + j * 64)));
+                roomObjects.add(new BrickObject(new Vec2(15 * block + i * 64, 10 + j * 64)));
+
+
+                //central platforms
+                roomObjects.add(new Platform(new Vec2(13 * block, 7 * block + 16)));
+                roomObjects.add(new Platform(new Vec2(10 * block, 10 * block)));
+                //todo fall through platforms
+                roomObjects.add(new Platform(new Vec2(10 * block, 13 * block + 16)));
+                roomObjects.add(new Platform(new Vec2(12 * block, 13 * block + 16)));
+                roomObjects.add(new Platform(new Vec2(12 * block, 13 * block + 16)));
+
+                //fall tghough again
+                roomObjects.add(new Platform(new Vec2(13 * block, 3 * block + 16)));
+                roomObjects.add(new Platform(new Vec2(10 * block, 1 * block + 16)));
+                roomObjects.add(new Platform(new Vec2(12 * block, 1 * block + 16)));
+
+
+            }
+        }
+    }
 }
+
 
 /*
     int block = 64;
@@ -22,35 +65,35 @@ public class RoomUDL  extends RoomTemplate {
         for (int j = 0; j < 5; j++){
 
         //bottom left bricks
-        addWorldObject(new BrickObject(new Vec2(0+i*64, 0+j*64)));
-        addWorldObject(new BrickObject(new Vec2(5*block+i*block,0*block+j*block )));
+        roomObjects.add(new BrickObject(new Vec2(0+i*64, 0+j*64)));
+        roomObjects.add(new BrickObject(new Vec2(5*block+i*block,0*block+j*block )));
         // top left bricks
-        addWorldObject(new BrickObject(new Vec2(0*block+i*64, 10*block+j*64)));
-        addWorldObject(new BrickObject(new Vec2(5*block+i*64, 10*block+j*64)));
+        roomObjects.add(new BrickObject(new Vec2(0*block+i*64, 10*block+j*64)));
+        roomObjects.add(new BrickObject(new Vec2(5*block+i*64, 10*block+j*64)));
 
 
         //right wall of bricks
-        addWorldObject(new BrickObject(new Vec2(15*block+i*64, 0+j*64)));
-        addWorldObject(new BrickObject(new Vec2(15*block+i*64, 5*block+j*64)));
-        addWorldObject(new BrickObject(new Vec2(20*block+i*64, 0+j*64)));
-        addWorldObject(new BrickObject(new Vec2(20*block+i*64, 5*block+j*64)));
-        addWorldObject(new BrickObject(new Vec2(20*block+i*64, 10*block+j*64)));
-        addWorldObject(new BrickObject(new Vec2(15*block+i*64, 10*block+j*64)));
-        addWorldObject(new BrickObject(new Vec2(15*block+i*64, 10+j*64)));
+        roomObjects.add(new BrickObject(new Vec2(15*block+i*64, 0+j*64)));
+        roomObjects.add(new BrickObject(new Vec2(15*block+i*64, 5*block+j*64)));
+        roomObjects.add(new BrickObject(new Vec2(20*block+i*64, 0+j*64)));
+        roomObjects.add(new BrickObject(new Vec2(20*block+i*64, 5*block+j*64)));
+        roomObjects.add(new BrickObject(new Vec2(20*block+i*64, 10*block+j*64)));
+        roomObjects.add(new BrickObject(new Vec2(15*block+i*64, 10*block+j*64)));
+        roomObjects.add(new BrickObject(new Vec2(15*block+i*64, 10+j*64)));
 
 
         //central platforms
-        addWorldObject(new Platform(new Vec2(13*block, 7*block+16)));
-        addWorldObject(new Platform(new Vec2(10*block, 10*block)));
+        roomObjects.add(new Platform(new Vec2(13*block, 7*block+16)));
+        roomObjects.add(new Platform(new Vec2(10*block, 10*block)));
         //todo fall through platforms
-        addWorldObject(new Platform(new Vec2(10*block, 13*block+16)));
-        addWorldObject(new Platform(new Vec2(12*block, 13*block+16)));
-        addWorldObject(new Platform(new Vec2(12*block, 13*block+16)));
+        roomObjects.add(new Platform(new Vec2(10*block, 13*block+16)));
+        roomObjects.add(new Platform(new Vec2(12*block, 13*block+16)));
+        roomObjects.add(new Platform(new Vec2(12*block, 13*block+16)));
 
         //fall tghough again
-        addWorldObject(new Platform(new Vec2(13*block, 3*block+16)));
-        addWorldObject(new Platform(new Vec2(10*block, 1*block+16)));
-        addWorldObject(new Platform(new Vec2(12*block, 1*block+16)));
+        roomObjects.add(new Platform(new Vec2(13*block, 3*block+16)));
+        roomObjects.add(new Platform(new Vec2(10*block, 1*block+16)));
+        roomObjects.add(new Platform(new Vec2(12*block, 1*block+16)));
 
 
         }
--- InfinityRun/core/src/com/tda367/infinityrun/Roomtemplates/RoomURD.java
+++ InfinityRun/core/src/com/tda367/infinityrun/Roomtemplates/RoomURD.java
@@ -1,5 +1,8 @@
 package com.tda367.infinityrun.Roomtemplates;
 
+import com.tda367.infinityrun.Math.Vec2;
+import com.tda367.infinityrun.RoomTiles.BrickObject;
+
 public class RoomURD  extends RoomTemplate {
     public RoomURD() {
         u =true;
@@ -7,18 +10,29 @@ public class RoomURD  extends RoomTemplate {
         d=true;
         l=false;
         roomExits = 3;
+        for (int x = 0; x < 10; x++) {
+            for (int y = 0; y < 5; y++) {
+                roomObjects.add(new BrickObject(new Vec2((24-x)*block,y*block)));
+                roomObjects.add(new BrickObject(new Vec2((24-x)*block,(13-y)*block)));
+            }
+        }
+        for (int x = 0; x < 10; x++) {
+            for (int y = 0; y < 14; y++) {
+                roomObjects.add(new BrickObject(new Vec2(x*block,y*block)));
+            }
+        }
     }
     /*
             int block = 64;
         for (int x = 0; x < 10; x++) {
             for (int y = 0; y < 5; y++) {
-                addWorldObject(new BrickObject(new Vec2((24-x)*block,y*block)));
-                addWorldObject(new BrickObject(new Vec2((24-x)*block,(13-y)*block)));
+                roomObjects.add(new BrickObject(new Vec2((24-x)*block,y*block)));
+                roomObjects.add(new BrickObject(new Vec2((24-x)*block,(13-y)*block)));
             }
         }
         for (int x = 0; x < 10; x++) {
             for (int y = 0; y < 14; y++) {
-                addWorldObject(new BrickObject(new Vec2(x*block,y*block)));
+                roomObjects.add(new BrickObject(new Vec2(x*block,y*block)));
             }
         }
      */
