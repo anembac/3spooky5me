94d80f472480135a9e498994f96e3bebb30ec59f
anembac <anembac@gmail.com>
2017-05-22 22:26:42 +0200

    Created basic shop, some issues remain

--- InfinityRun/core/src/com/tda367/infinityrun/Shop.java
+++ InfinityRun/core/src/com/tda367/infinityrun/Shop.java
@@ -1,8 +1,49 @@
 package com.tda367.infinityrun;
 
-/**
- * Created by kaffe on 4/3/17.
- */
-public class
-Shop {
+
+import java.util.HashMap;
+
+public class Shop {
+    private Character shopOwner;
+    private HashMap<String, Upgrade> upgList;
+
+
+    public Shop(Character currentChar){
+        shopOwner = currentChar;
+        upgList = shopOwner.getUpgrades();
+    }
+
+    public void purchaseUpgrade(String upgName){
+        Upgrade currentUpg = upgList.get(upgName);
+        System.out.println("Name: " + upgName + ", level: " + currentUpg.getLevel());
+        System.out.println("Coins: " + shopOwner.getCoins() + ", Cost: " + getPrice(currentUpg));
+        if(getPrice(currentUpg)<=shopOwner.getCoins()){
+            currentUpg.addLevel();
+            System.out.println(upgName + ": " + currentUpg.getLevel());
+        }
+    }
+
+    public int getPrice(Upgrade upg){
+        return upg.getBasePrice()*upg.getLevel();
+    }
+
+    public HashMap<String, Upgrade> getUpgList(){
+        return upgList;
+    }
+
+    public String[] getUpgNameList(){
+        int len = getUpgList().keySet().size();
+        String[] nameList = new String[len];
+        for(int i = 0; i<len;i++){
+            nameList[i] = (String)getUpgList().keySet().toArray()[i];
+        }
+        return nameList;
+    }
+
+
+
+
+
+
+
 }
---
+++ InfinityRun/core/src/com/tda367/infinityrun/ShopScreen.java
@@ -0,0 +1,107 @@
+package com.tda367.infinityrun;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+
+public class ShopScreen implements Screen {
+
+    Table upgradeTable;
+    Shop shop;
+    Stage shopStage = new Stage();
+    BitmapFont font = new BitmapFont();
+    SpriteBatch batch = new SpriteBatch();
+    OrthographicCamera camera = new OrthographicCamera();
+    TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("testpack1.pack"));
+    TextureRegion textureUp = new TextureRegion(atlas.findRegion("testtexture"));
+    TextureRegion textureDown = new TextureRegion(atlas.findRegion("testtexture2"));
+    TextureRegionDrawable textureUpDrawable = new TextureRegionDrawable(textureUp);
+    TextureRegionDrawable textureDownDrawable = new TextureRegionDrawable(textureDown);
+    Button.ButtonStyle style = 
+            new Button.ButtonStyle(textureUpDrawable, textureDownDrawable, textureUpDrawable);
+    Button[] buttonArray;
+    String[] nameList;
+
+
+
+    public ShopScreen(Shop shop){
+        this.shop = shop;
+        nameList = shop.getUpgNameList();
+        upgradeTable = new Table();
+        buttonArray = new Button[shop.getUpgList().size()];
+        for(int i = 0; i < buttonArray.length; i++){
+            buttonArray[i] = new Button(style);
+            upgradeTable.add(buttonArray[i]);
+        }
+        camera.setToOrtho(false, 1600, 900);
+        upgradeTable.setPosition(1600/2-upgradeTable.getMaxWidth(), 900/2-upgradeTable.getMaxHeight());
+        shopStage.addActor(upgradeTable);
+        Gdx.input.setInputProcessor(shopStage);
+
+    }
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        buttonClickedCheck();
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        camera.update();
+        batch.begin();
+        shopStage.draw();
+
+        //Batch&Font rendering
+        batch.setProjectionMatrix(camera.combined);
+        upgradeTable.draw(batch, 1);
+        batch.end();
+    }
+
+
+    private void buttonClickedCheck(){
+        for(int i = 0; i<buttonArray.length;i++){
+            if(buttonArray[i].isPressed()){
+                shop.purchaseUpgrade(nameList[i]);
+            }
+        }
+    }
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        shopStage.dispose();
+        atlas.dispose();
+
+
+    }
+}
