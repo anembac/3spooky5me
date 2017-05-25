4127007f66b89d077c2d7b56cb83c9efc805557a
mintuw <jacob.rohdin@gmail.com>
2017-05-25 11:15:45 +0200

    Added so the looting works.

--- InfinityRun/core/src/com/tda367/infinityrun/MeleeWeapon.java
+++ InfinityRun/core/src/com/tda367/infinityrun/MeleeWeapon.java
@@ -10,71 +10,63 @@ import java.util.List;
  */
 
 public class MeleeWeapon extends WorldObject {
-     protected final double damage;
-     protected final double CD;
-     protected final double range;
-     private double currentCD = 0;
+    protected final double damage;
+    protected final double CD;
+    protected final double range;
+    private double currentCD = 0;
 
-     public MeleeWeapon (int damage, double CD, double range) {
-         super(new Vec2(/*offset from character*/32,26), new Vec2(/*size*/164,12));
-         setTexture("WorldObjects/weapon.png");
-         setCollidable(false);
-         this.damage = damage;
-         this.CD = CD;
-         this.range = range;
-     }
+    public MeleeWeapon(int damage, double CD, double range) {
+        super(new Vec2(/*offset from character*/32, 26), new Vec2(/*size*/164, 12));
+        setTexture("WorldObjects/weapon.png");
+        setCollidable(false);
+        this.damage = damage;
+        this.CD = CD;
+        this.range = range;
+    }
 
-     public double getDamage(){
-         return damage;
-     }
+    public double getDamage() {
+        return damage;
+    }
 
-     public double getCD(){
-         return CD;
-     }
+    public double getCD() {
+        return CD;
+    }
 
-     public double getRange(){
-         return range;
-     }
+    public double getRange() {
+        return range;
+    }
 
-     public void setDirRight()
-     {
-         position.x = 32;
-     }
+    public void setDirRight() {
+        position.x = 32;
+    }
 
-     public  void setDirLeft()
-     {
-         position.x = -32;
-     }
+    public void setDirLeft() {
+        position.x = -32;
+    }
 
     @Override
     public void frame(float dt, float heroX, float heroY, InputState state) {
         super.frame(dt, heroX, heroY, state);
-        currentCD = Math.max(0, currentCD-dt);
+        currentCD = Math.max(0, currentCD - dt);
 
-        if(state.attackPressed())
-        {
+        if (state.attackPressed()) {
             // temp animation to see when we are auctually attacking
-            position.y = (position.y-1) % 30 +21;
-        }
-        else position.y = 16.0f;
+            position.y = (position.y - 1) % 30 + 21;
+        } else position.y = 16.0f;
 
-        if(state.attackPressed() && currentCD < 0.001)
-        {
+        if (state.attackPressed() && currentCD < 0.001) {
+            List<WorldObject> output = CollisionManager.getInstance().getKNearest(this, 10); // get the 10 nearest
 
-            List<WorldObject> output= CollisionManager.getInstance().getKNearest(this, 10); // get the 10 nearest
-
-            for(WorldObject wo : output)
-            {
-                if(wo != this.getParent() && Utils.distance(this.getParent(), wo) < (range * Constants.meter) && wo instanceof LivingObject)
-                {
-                    ((LivingObject)wo).damage(damage);
-                    ((LivingObject)wo).acceleration.y = 400;
-                    if (position.x >  0){
-                    ((LivingObject)wo).acceleration.x = 700;}
-                    if (position.x <  0){
-                        ((LivingObject)wo).acceleration.x = -700;
+            for (WorldObject wo : output) {
+                if (wo != this.getParent() && Utils.distance(this.getParent(), wo) < (range * Constants.meter) && wo instanceof LivingObject) {
+                    ((LivingObject) wo).damage(damage);
+                    ((LivingObject) wo).acceleration.y = 400;
+                    if (position.x > 0) {
+                        ((LivingObject) wo).acceleration.x = 700;
                     }
-
+                    if (position.x < 0) {
+                        ((LivingObject) wo).acceleration.x = -700;
+                    }
                     currentCD = CD;
                 }
             }
--- InfinityRun/core/src/com/tda367/infinityrun/Upgrades/Looting.java
+++ InfinityRun/core/src/com/tda367/infinityrun/Upgrades/Looting.java
@@ -15,25 +15,4 @@ public class Looting extends Upgrade {
         perLevelMultiplier = 5;
         basePrice = 10;
     }
-
-
-    /*
-    This method determines how many coins you will get, 200% would equal 2 coins.
-    250% equals 2 coins and a 50% chance to get an extra coin.
-    */
-    public int numberOfCoins() {
-        int coins = ((int) (getValueDouble() * 100)) / 100;
-        if (extraCoin(((int) (getValueDouble() * 100)) % 100)) {
-            return (coins + 1);
-        } else {
-            return coins;
-        }
-    }
-
-    //Determines if you will get the extra coin based on you extra percentage chance.
-    private boolean extraCoin(int chance) {
-        Random rnd = new Random();
-        return chance >= (rnd.nextInt(100) + 1);
-    }
-
 }
