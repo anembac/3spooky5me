8349d5fbb895967ccef93136997f74b3f9441ce0
anembac <anembac@gmail.com>
2017-07-05 11:58:27 +0200

    Moved Constants from Model to Utils

--- InfinityRun/core/src/com/tda367/infinityrun/Utils/Constants.java
+++ InfinityRun/core/src/com/tda367/infinityrun/Utils/Constants.java
@@ -1,8 +1,5 @@
-package com.tda367.infinityrun.Model;
+package com.tda367.infinityrun.Utils;
 
-/**
- * Created by Mikael on 5/19/2017.
- */
 public class Constants {
     public static final int meter = 64;
     public static final int roomHeight = 15;
