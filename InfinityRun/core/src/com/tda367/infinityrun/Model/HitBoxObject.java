34eaf7efe0e431afb210b87225a4a0c0baad2bb3
anembac <anembac@gmail.com>
2017-07-27 11:43:21 +0200

    Created hitbox object

---
+++ InfinityRun/core/src/com/tda367/infinityrun/Model/HitBoxObject.java
@@ -0,0 +1,13 @@
+package com.tda367.infinityrun.Model;
+
+import com.tda367.infinityrun.Utils.Math.Vec2;
+
+public class HitBoxObject extends WorldObject{
+
+
+    public HitBoxObject(Vec2 position, Vec2 bounds) {
+        super(position, bounds);
+        setCollidable(false);
+    }
+
+}
